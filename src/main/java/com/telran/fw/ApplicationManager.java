54c86cec336cc4b5e65015c096225ae49df2f16b
TayaHatum <21taya21@gmail.com>
2021-02-15 20:57:22 +0200

    Added Listener and Login

--- src/main/java/com/telran/fw/ApplicationManager.java
+++ src/main/java/com/telran/fw/ApplicationManager.java
@@ -3,13 +3,18 @@ package com.telran.fw;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.events.EventFiringWebDriverFactory;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -35,8 +40,9 @@ public class ApplicationManager {
         driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        logger.info("App version: " );
+        driver= EventFiringWebDriverFactory.getEventFiringWebDriver(driver,new AppiumListener());
         userHelper = new UserHelper(driver);
+
     }
 
     public void stop() {
--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,9 +1,20 @@
+import com.telran.model.User;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class LoginTest extends TestBase{
+public class LoginTest extends TestBase {
+
+
+    @BeforeMethod
+    public void preconditions() {
+        app.user().skipWizard();
+        app.user().goToLoginForm();
+    }
 
     @Test
-    public void testLogin(){
+    public void testLogin() {
+        app.user().fillLoginForm(new User().withfName("hatum.testing").withPassword("Hatum12344$"));
+        app.user().clickLoginButton();
 
     }
 }
